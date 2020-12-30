cf9e390ffa4b517cde17ef85aa8c634e5afeabcf
Darya Alferova <darka83@mail.ru>
2020-12-30 16:13:17 +0300

    Move go_to_login_page and should_be_login_link from MainPage to BasePage.

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators:
     COST_BASKET = (By.CSS_SELECTOR, ".basket-mini")
     BOOK_PRICE = (By.CSS_SELECTOR, ".product_main>p")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,6 @@
 """
 pytest -v -s --tb=line --language=en test_product_page.py
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-@pytest.mark.parametrize('promo_offer', ["0", "1", "3", "4", "5", "6", "7", "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+pytest -v -s --tb=line --language=en test_product_page.py::test_guest_can_go_to_login_page_from_product_page
 """
 from .pages.product_page import ProductPage
 import pytest
@@ -45,3 +42,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_to_basket()
     product_page.should_be_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
